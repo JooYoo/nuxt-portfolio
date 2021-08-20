154ea4d6f0216ffc8c9fbdb55423749912719825
JooYoo <jooyoo@outlook.com>
2021-08-20 15:48:42 +0200

    feat: added yu-project cards example

--- components/YuProject.vue
+++ components/YuProject.vue
@@ -1,5 +1,51 @@
 <template>
-  <v-container>
-    <h3 class="text-h3 font-weight-thin mt-10">Project</h3>
+  <v-container class="my-10">
+    <h3 class="text-h3 font-weight-thin mb-10">Project</h3>
+
+    <v-row>
+      <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+        <v-card elevation="8" outlined hover>
+          <v-img
+            :src="card.src"
+            class="white--text"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+          >
+            <v-card-title v-text="card.title"></v-card-title>
+          </v-img>
+
+          <v-card-actions class="justify-center">
+            <v-btn v-for="iconName of icons" :key="iconName" icon class="mx-3">
+              <v-icon v-text="iconName">mdi-heart</v-icon>
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    cards: [
+      {
+        title: 'House',
+        src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg',
+        flex: 12
+      },
+      {
+        title: 'Road',
+        src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg',
+        flex: 6
+      },
+      {
+        title: 'Airline',
+        src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg',
+        flex: 6
+      }
+    ],
+    icons: ['mdi-heart', 'mdi-bookmark', 'mdi-share-variant']
+  })
+}
+</script>
