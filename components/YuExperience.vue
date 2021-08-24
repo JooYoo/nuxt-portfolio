f2c66e98956872949bab6afa9885a00cd5b3f239
JooYoo <jooyoo@outlook.com>
2021-08-24 10:13:04 +0200

    feat: yu-experience timeline

--- components/YuExperience.vue
+++ components/YuExperience.vue
@@ -1,5 +1,43 @@
 <template>
-  <v-container class="py-10">
+  <v-container class="py-15">
     <h3 class="text-h3 font-weight-thin">Experience</h3>
+
+    <v-timeline class="mt-10">
+      <v-timeline-item
+        v-for="(year, i) in experiences"
+        :key="i"
+        :color="year.color"
+        small
+      >
+        <span
+          slot="opposite"
+          :class="`headline font-weight-bold ${year.color}--text`"
+          v-text="year.year"
+        ></span>
+        <div class="py-3">
+          <h2
+            :class="`headline font-weight-light mb-2 ${year.color}--text`"
+            v-text="year.title"
+          ></h2>
+          <div
+            :class="`subheading font-weight-light mb-1 ${year.color}--text `"
+            v-text="year.where"
+          ></div>
+          <div>{{ year.content.join(' ') }}</div>
+        </div>
+      </v-timeline-item>
+    </v-timeline>
   </v-container>
 </template>
+
+<script>
+import JsonExperiences from '~/static/experience.json'
+
+export default {
+  name: 'YuExperience',
+
+  data: () => ({
+    experiences: JsonExperiences
+  })
+}
+</script>
