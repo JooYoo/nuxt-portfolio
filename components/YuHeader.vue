75dd5a34919c31af4f94baacdce2f1f2b3bdfe07
JooYoo <jooyoo@outlook.com>
2021-09-07 05:15:32 +0200

    refactor: yu-header links

--- components/YuHeader.vue
+++ components/YuHeader.vue
@@ -4,24 +4,36 @@
       <v-btn text class="text-h4" v-text="'👨🏻‍💻'" />
     </NuxtLink>
     <v-spacer></v-spacer>
-    <!-- TODO: iteration nav-btns -->
-    <v-btn icon>
-      <NuxtLink class="no-deco" to="/">
-        <v-icon>mdi-home</v-icon>
+
+    <v-btn v-for="link of links" :key="link.to" icon>
+      <NuxtLink :to="link.to" class="no-deco">
+        <v-icon>{{ link.icon }}</v-icon>
       </NuxtLink>
-    </v-btn>
-    <v-btn icon>
-      <NuxtLink class="no-deco" to="/project">
-        <v-icon>mdi-flask-empty-outline</v-icon>
-      </NuxtLink>
-    </v-btn>
-    <v-btn icon>
-      <NuxtLink class="no-deco" to="/gotcha">
-        <v-icon>mdi-lightning-bolt</v-icon>
-      </NuxtLink>
-    </v-btn>
-    <v-btn icon>
-      <v-icon>mdi-music-circle-outline</v-icon>
     </v-btn>
   </v-app-bar>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    links: [
+      {
+        to: '/',
+        icon: 'mdi-home'
+      },
+      {
+        to: '/project',
+        icon: 'mdi-flask-empty-outline'
+      },
+      {
+        to: '/gotcha',
+        icon: 'mdi-lightning-bolt'
+      },
+      {
+        to: '/music',
+        icon: 'mdi-music-circle-outline'
+      }
+    ]
+  })
+}
+</script>
---
+++ pages/music/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <v-main>
+    <v-container>
+      <h3>Music Page</h3>
+    </v-container>
+  </v-main>
+</template>
