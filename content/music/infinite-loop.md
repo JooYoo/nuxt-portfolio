af572de3be7314785b1ef6c23f110d7c4374451b
JooYoo <jooyoo@outlook.com>
2021-09-07 05:50:36 +0200

    chore: music post route

---
+++ content/music/infinite-loop.md
@@ -0,0 +1,8 @@
+---
+title: Infinite Loop
+time: 2021
+slug: infinite-loop
+---
+
+<h1 align="center">Infinite Loop</h1>
+<div align="center">2021</div>
---
+++ pages/music/_slug.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-main>
+    <v-container>
+      <nuxt-content :document="musicPost" />
+    </v-container>
+  </v-main>
+</template>
+
+<script>
+export default {
+  async asyncData({ $content, params }) {
+    const musicPost = await $content('music', params.slug).fetch()
+
+    return { musicPost }
+  }
+}
+</script>
