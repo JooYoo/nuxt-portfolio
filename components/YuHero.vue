558640413e95335f6f489bdd77e5ced32790edd3
JooYoo <jooyoo@outlook.com>
2021-09-10 06:28:33 +0200

    style: yu-hero txt

--- components/YuHero.vue
+++ components/YuHero.vue
@@ -1,14 +1,48 @@
 <template>
-  <v-parallax
-    dark
-    src="https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg"
-  >
+  <v-parallax dark height="800" :src="bg">
     <v-row align="center" justify="center">
       <v-col class="text-center" cols="12">
-        <h1 class="text-h3 font-weight-medium mb-4">Zhu Yu</h1>
-        <h4 class="font-weight-light">between Code and Design</h4>
-        <h4 class="subheading">Frontend Developer</h4>
+        <h1 class="xiaowei title-name text-h2 font-weight-medium mb-12">
+          Z h u Y u
+        </h1>
+        <h4 class="xiaowei title-slogan text-h6 font-weight-thin">
+          between Code and Design
+        </h4>
+        <hr class="divider my-6" />
+        <h4 class="xiaowei title-job">Frontend Developer</h4>
       </v-col>
     </v-row>
   </v-parallax>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    bg: 'https://cdn.vuetifyjs.com/images/backgrounds/vbanner.jpg'
+  })
+}
+</script>
+
+<style lang="scss" scoped>
+@import url('https://fonts.googleapis.com/css?family=ZCOOL+XiaoWei');
+
+.xiaowei {
+  font-family: 'ZCOOL XiaoWei', sans-serif !important;
+}
+
+.title-name {
+  font-size: 65px !important;
+}
+
+.divider {
+  margin: auto;
+  border: none;
+  height: 1px;
+  width: 90%;
+  background-color: rgba(255, 255, 255, 0.5);
+}
+
+.title-job {
+  text-shadow: 2px 2px 4px #000;
+}
+</style>
