644372aa417a8c48e75588c78f99875e2e5f9e30
JooYoo <jooyoo@outlook.com>
2021-09-21 00:16:59 +0200

    refactor: yu-skill-card

--- components/YuSkill.vue
+++ components/YuSkill.vue
@@ -1,88 +1,56 @@
 <template>
   <v-container class="py-15">
     <h3 class="text-h3 font-weight-thin">Skill</h3>
-
     <v-row>
       <v-col>
-        <v-card
-          class="mx-auto mt-10 py-5 rounded-lg"
-          outlined
-        >
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-h5 mb-1">
-                Frontend
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                My go-to tool at the work is <strong>Vue</strong> and it's ecosystem. I'm also a big fan of <strong>React</strong>.
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider class="mx-4 my-4"></v-divider>
-          <v-card-actions>
-            <img
-              v-for="(techLogo, i) of frontendSkills"
-              :key="i"
-              :src="require(`~/assets/icon/tech/${techLogo.name}.svg`)"
-              :title="techLogo.name"
-              height="50px"
-              class="mx-5"
-            />
-          </v-card-actions>
-        </v-card>
+        <WidgetsYuSkillCard
+          :title="frontendSkill.title"
+          :subtitle="frontendSkill.subtitle"
+          :skills="frontendSkill.techlogos"
+        />
 
-        <v-card
-          class="mx-auto mt-10 py-5 rounded-lg"
-          outlined
-        >
-          <v-list-item>
-            <v-list-item-content>
-              <v-list-item-title class="text-h5 mb-1">
-                Backend
-              </v-list-item-title>
-              <v-list-item-subtitle>
-                I'm using <strong>C#</strong> at the work. <strong>Nodejs</strong> is my choice when I develop my own project.
-              </v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider class="mx-4 my-4"></v-divider>
-          <v-card-actions>
-            <img
-              v-for="(techLogo, i) of backendSkills"
-              :key="i"
-              :src="require(`~/assets/icon/tech/${techLogo.name}.svg`)"
-              :title="techLogo.name"
-              height="50px"
-              class="mx-5"
-              :style="techLogo.name==='Express'?'height:30px':''"
-            />
-          </v-card-actions>
-        </v-card>
+        <WidgetsYuSkillCard
+          :title="backendSkill.title"
+          :subtitle="backendSkill.subtitle"
+          :skills="backendSkill.techlogos"
+        />
       </v-col>
     </v-row>
   </v-container>
 </template>
 
 <script>
+// TODO: refactor: save as json file
 export default {
   data: () => ({
-    frontendSkills: [
-      { name: 'Vue' },
-      { name: 'Nuxtjs' },
-      { name: 'Vuetify' },
-      { name: 'React' },
-      { name: 'Redux' },
-      { name: 'Sass' },
-      { name: 'ionic' },
-      // { name: 'VuePress' },
-    ],
-    backendSkills: [
-      { name: 'Nodejs' },
-      { name: 'MongoDB' },
-      { name: 'csharp' },
-      { name: 'dotnet' },
-      { name: 'Express' },
-    ],
+    frontendSkill: {
+      title: 'Frontend',
+      subtitle:
+        "My go-to tool at the work is <strong>Vue</strong> and it's ecosystem. I'm also a big fan of <strong>React</strong>.",
+      techlogos: [
+        { name: 'Vue' },
+        { name: 'Nuxtjs' },
+        { name: 'Vuetify' },
+        { name: 'React' },
+        { name: 'Redux' },
+        { name: 'Sass' },
+        { name: 'ionic' },
+        // { name: 'VuePress' },
+      ],
+    },
+
+    backendSkill: {
+      title: 'Backend',
+      subtitle:
+        "I'm using <strong>C#</strong> at the work. <strong>Nodejs</strong> is my choice when I develop my own project.",
+      techlogos: [
+        { name: 'csharp' },
+        { name: 'dotNetCore' },
+        { name: 'Nodejs' },
+        { name: 'MongoDB' },
+        { name: 'Express' },
+      ],
+    },
   }),
 }
 </script>
---
+++ components/widgets/YuSkillCard.vue
@@ -0,0 +1,42 @@
+<template>
+  <v-card
+    class="mx-auto mt-10 py-5 rounded-lg"
+    outlined
+  >
+    <v-list-item>
+      <v-list-item-content>
+        <v-list-item-title
+          class="text-h5 mb-1"
+          v-text="title"
+        />
+        <!-- eslint-disable vue/no-v-html -->
+        <v-list-item-subtitle
+          class="mt-2"
+          v-html="subtitle"
+        />
+      </v-list-item-content>
+      <!--eslint-enable-->
+    </v-list-item>
+    <v-divider class="mx-4 my-3"></v-divider>
+    <v-card-actions>
+      <img
+        v-for="(skill, i) of skills"
+        :key="i"
+        :src="require(`~/assets/icon/tech/${skill.name}.svg`)"
+        :title="skill.name"
+        :style="skill.name==='Express'?'height:30px':''"
+        height="50px"
+        class="mx-5"
+      />
+    </v-card-actions>
+  </v-card>
+
+</template>
+
+<script>
+export default {
+  // eslint-disable-next-line vue/require-prop-types
+  props: ['title', 'subtitle', 'skills'],
+}
+</script>
+
