bb26a7b80bc60c42dc7c92d0b1ffad8526540d07
JooYoo <jooyoo@outlook.com>
2021-09-30 11:35:03 +0200

    style: gotcha-list hover

--- pages/gotcha/index.vue
+++ pages/gotcha/index.vue
@@ -1,24 +1,30 @@
 <template>
   <v-main>
-    <v-container>
-      <v-list>
-        <v-list-item v-for="post of gotchaPosts" :key="post.title">
-          <NuxtLink
-            :to="`gotcha/${post.slug}`"
-            class="no-deco list-item--split"
-          >
-            <v-list-item-content>
-              <v-list-item-title v-text="post.title" />
-              <v-list-item-subtitle v-text="post.keyword" />
-            </v-list-item-content>
 
-            <v-list-item-action>
-              <v-list-item-action-text v-text="post.date" />
-            </v-list-item-action>
-          </NuxtLink>
-        </v-list-item>
-      </v-list>
-    </v-container>
+    <v-list>
+
+      <v-list-item
+        v-for="post of gotchaPosts"
+        :key="post.title"
+        class="px-10 list-item--hover"
+      >
+        <NuxtLink
+          :to="`gotcha/${post.slug}`"
+          class="no-deco list-item--split"
+        >
+          <v-list-item-content>
+            <v-list-item-title v-text="post.title" />
+            <v-list-item-subtitle v-text="post.keyword" />
+          </v-list-item-content>
+
+          <v-list-item-action>
+            <v-list-item-action-text v-text="post.date" />
+          </v-list-item-action>
+        </NuxtLink>
+      </v-list-item>
+
+    </v-list>
+
   </v-main>
 </template>
 
@@ -31,9 +37,9 @@ export default {
       .fetch()
 
     return {
-      gotchaPosts
+      gotchaPosts,
     }
-  }
+  },
 }
 </script>
 
@@ -42,5 +48,11 @@ export default {
   display: flex;
   align-items: center;
   flex: 1 1 100%;
+}
+
+.list-item--hover {
+  &:hover {
+    background-color: rgba(255, 255, 255, 0.05);
+  }
 }
 </style>
