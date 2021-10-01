3225be86034ffe5c89220e9bb3998585a78f9495
JooYoo <jooyoo@outlook.com>
2021-10-01 11:50:29 +0200

    feat: music-links

--- pages/music/index.vue
+++ pages/music/index.vue
@@ -1,6 +1,23 @@
 <template>
   <v-main>
     <v-container>
+      <div class="d-flex justify-space-around my-15">
+        <a
+          v-for="link of musicLinks"
+          :key="link.name"
+          target="_blank"
+          :href="link.url"
+          class="d-flex align-center no-deco"
+        >
+          <v-img
+            :src="require(`~/assets/icon/music/${link.logo}`)"
+            width="25"
+            class="mr-2"
+          />
+          {{link.name}}
+        </a>
+
+      </div>
       <v-row>
         <v-col
           v-for="post in musicPosts"
@@ -8,7 +25,10 @@
           class="d-flex child-flex"
           cols="4"
         >
-          <NuxtLink :to="`music/${post.slug}`" class="no-deco music-cover">
+          <NuxtLink
+            :to="`music/${post.slug}`"
+            class="no-deco music-cover"
+          >
             <v-img
               :src="require(`~/assets/album/${post.cover}`)"
               aspect-ratio="1"
@@ -31,9 +51,33 @@ export default {
       .fetch()
 
     return {
-      musicPosts
+      musicPosts,
     }
-  }
+  },
+  data: () => ({
+    musicLinks: [
+      {
+        name: 'Spotify',
+        logo: 'spotify.svg',
+        url: 'https://open.spotify.com/artist/0pzeFK0JIwNYbAXiItjGB3',
+      },
+      {
+        name: 'SoundCloud',
+        logo: 'soundcloud.svg',
+        url: 'https://soundcloud.com/joo-yoo-965283946',
+      },
+      {
+        name: 'AppleMusic',
+        logo: 'applemusic.svg',
+        url: 'https://music.apple.com/us/artist/lofi-coder/1584127276',
+      },
+      {
+        name: 'AmazonMusic',
+        logo: 'amazonmusic.svg',
+        url: 'https://music.amazon.de/artists/B09F9ZRQY6/lofi-coder',
+      },
+    ],
+  }),
 }
 </script>
 
